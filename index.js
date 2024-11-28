763bff5574dfe10462108214087e4042f635818e
Brán Balázs Antal <81647160+Blzs2005@users.noreply.github.com>
2024-11-28 16:52:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let generateButton = document.getElementById("button");
+let usableCharacters = characters;
+let password1 = document.getElementById("output1");
+let password2 = document.getElementById("output2");
+
+generateButton.addEventListener("click", function () {
+  function generatePassword() {
+    let password = "";
+    for (let i = 0; i < 15; i++) {
+      let randomCharacter = Math.floor(Math.random() * usableCharacters.length);
+      password += usableCharacters[randomCharacter];
+    }
+    return password;
+  }
+
+  password1.textContent = generatePassword();
+  password2.textContent = generatePassword();
+});
